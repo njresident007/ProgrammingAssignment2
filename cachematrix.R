6a6ec775b45a00e14d42c63cf5e59f9f46c60fb2
Yelena Cherdak <yelenacherdak@yahoo.com>
2015-12-27 15:29:49 -0500

    Programming, Programming Assignment 2 by Y.Cherdak

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Developed by Yelena Cherdak
+## This function is:
+## This function creates adds list of functions to the "matrix" object supplied by parameter
+## using R S3 scope mechanism it allowes to the user to access cached values of x and matrixInv objects
+##
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  matrixInv <- NULL
+  set <- function(y) {
+    x <<- y
+    matrixInv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inv) matrixInv <<- inv
+  getinverse<- function() matrixInv
+  list( set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## this function checks if object of type makeCacheMatrix has inverse value 
+## set in cache
+## if the value is not set, functions sets it
+## return type: inverse matrix 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverseM = NULL
+  inverseM <- x$getinverse
+  if(is.null (inverseM) & !is.null(x)) {
+    inverseM <- solve(x)
+    x.setinverse(inverseM)
+  }
+  return(inverseM)
 }
